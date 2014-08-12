b37f98208b998cbf0db015ddd17733c4a1463d64
Chidiebere Nnadi <chidiebere.nnadi@gmail.com>
2014-08-12 13:38:42 +0100

    Added new comment model as well as completed the blog deletion functionaility

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,12 +2,15 @@ class ArticlesController < ApplicationController
 	def index
 		@articles = Article.all
 	end
+
 	def new
 		@article = Article.new
 	end
+
 	def edit
 		@article = Article.find(params[:id])
 	end
+
 	def update
 		@article = Article.find(params[:id])
 
@@ -18,6 +21,7 @@ class ArticlesController < ApplicationController
 		end
 
 	end
+
 	def create
 		@article = Article.new(article_params)
 
@@ -27,8 +31,16 @@ class ArticlesController < ApplicationController
 			render 'new'
 		end
 	end
+
 	def show
 		@article = Article.find(params[:id])
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		redirect_to articles_path
 	end
 
 	private
